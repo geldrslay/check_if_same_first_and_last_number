3f0b511f8a4397c6b778468e92db118f33e56eb8
Angela Ferrer <gelferrero01@gmail.com>
2024-01-25 02:04:14 +0800

    Defining function

---
+++ exercise_five.py
@@ -0,0 +1,10 @@
+# Check if the first and last number of a list is the same
+# Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+# Defining function 
+
+def check_if_same_first_and_last(number_list):
+    print ('The given list is:', number_list)
+    # Get the first and last number from the given list
+    first_number = number_list[0]
+    last_number = number_list [-1]
